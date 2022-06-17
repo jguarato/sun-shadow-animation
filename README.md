31141d4a3b5ea0d57426bebb85623a9f5041fa1e
Jessica Guarato <82293939+jguarato@users.noreply.github.com>
2022-06-17 16:11:45 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+## 🌞 Sunrise to sunset animation
+
+* A web application developed with JavaScript;
+* There is an animation of the environment depending on the position of the sun;
+* A range input controls the movement of the sun, one can choose from sunrise to sunset;
+* As the sun moves, one can see that the sky changes color, and the tree's shadow changes as well;
+* A Pen created on CodePen.io for the <strong>#CodePenChallenge: Positive and Negative</strong>;
+* Original URL: [https://codepen.io/jguarato/full/JjpxrRX](https://codepen.io/jguarato/full/JjpxrRX).
---
+++ dist/script.js
@@ -0,0 +1,53 @@
+const slider = document.querySelector('#sliderRange');
+
+slider.oninput = function() {
+  moveSun(this.value);
+  changeShadow(this.value);
+  changeSkyColor(this.value);
+}
+
+function moveSun(sunAngle) {
+  const sun = document.querySelector('#sun');
+  sun.style.transform = `rotate(${sunAngle}deg)`;
+}
+
+function changeShadow(sunAngle) {
+  
+  const fixedTransform = 'translatey(-84px) rotatex(75deg) rotatez(180deg)';
+   
+  const skewAngle = Math.abs(sunAngle) > 30 ? 
+      - sunAngle + (Math.sign(sunAngle) * 30) : - sunAngle / 2;
+  const skewAngleRad = skewAngle * Math.PI / 180;
+  
+  const tree = document.querySelector('#tree');
+  const treeTop = tree.getBoundingClientRect().top;
+  const treeBottom = tree.getBoundingClientRect().bottom;
+  const treeMidHeight = (treeTop - treeBottom) / 2;
+
+  let translatePos = treeMidHeight * Math.tan(skewAngleRad);
+  translatePos *= 0.82;
+  
+  const shadow = document.querySelector('#shadow');
+  shadow.style.transform = fixedTransform +
+    ` translatex(${translatePos}px)` +
+    ` skew(${skewAngle}deg, 0deg)`;
+  
+}
+
+function changeSkyColor(sunAngle) {
+  
+  const background = document.querySelector('.background');
+  const sky = document.querySelector('.sky');
+  
+  let blueSunrise = 100 * (1 + sunAngle / 180) + 30;
+  let blueSunset = 100 * (1 - sunAngle / 180) + 30;
+
+  if (sunAngle > 0) {
+    background.style.background = `black`;
+    sky.style.background = `linear-gradient(180deg, rgba(63,166,215,${blueSunset / 100}) ${blueSunset}%, rgba(253,132,58,1) 100%)`;
+  } else {
+    background.style.background = `white`;
+    sky.style.background = `linear-gradient(180deg, rgba(63,166,215,${blueSunrise / 100}) ${blueSunrise}%, rgba(255,235,169,1) 100%)`;
+  }
+  
+}
